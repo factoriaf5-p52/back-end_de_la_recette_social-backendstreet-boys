b98ff8f61781d845a33456c939be1d11559e707d
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2023-01-20 13:40:54 +0100

    created schaffold

---
+++ recette-social/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('recette/v1');
+  const options = new DocumentBuilder()
+    .setTitle('Recette-Social')
+    .setDescription('Api rest para recetas y red social')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app , document);
+
+  await app.listen(3000);
+}
+bootstrap();
