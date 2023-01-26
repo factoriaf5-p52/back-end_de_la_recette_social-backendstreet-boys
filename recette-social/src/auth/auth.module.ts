df34b11fafed72e80a21bb4a7d918a56bc4a7c35
ChaanChanChan <alexandertakepics@gmail.com>
2023-01-26 15:29:37 +0100

    login and jwt done

--- recette-social/src/auth/auth.module.ts
+++ recette-social/src/auth/auth.module.ts
@@ -3,11 +3,23 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/users/schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './entities/jwt.constants';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema
+      }
+    ]),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
+
   controllers: [AuthController],
   providers: [AuthService]
 })
--- recette-social/src/auth/auth.service.ts
+++ recette-social/src/auth/auth.service.ts
@@ -5,23 +5,30 @@ import { LoginAuthDto } from './dto/login-auth.dto';
 import { RegisterAuthDto } from './dto/register-auth.dto';
 import { hash, compare } from 'bcrypt';
 import { InjectModel } from '@nestjs/mongoose';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    private jwtService: JwtService
   ) { }
 
   async register(userObject: RegisterAuthDto) {
+    //destructuring password and setting onto userObject
     const { password } = userObject;
+    //encrypt password
     const plainToHash = await hash(password, 10);
+    //replace password to encrypted password
     userObject = { ...userObject, password: plainToHash };
+    //create user on db
     return this.userModel.create(userObject)
   }
 
   async login(userObjectLogin: LoginAuthDto) {
     //setting email and password into and object
     const { email, password } = userObjectLogin
+
     //find the user by email, if no user match the email return 404
     const findUser = await this.userModel.findOne({ email })
     if (!findUser) throw new HttpException('USER_NOT_FOUND', 404)
@@ -31,10 +38,15 @@ export class AuthService {
     //if dosent match retunr 403
     if (!checkPassword) throw new HttpException('PASSWORD_INVALID', 403)
 
-    //
-    const data = findUser;
-    console.log(data)
+    //creating a token and setting the user name and id into payload
+    const payload = { id: findUser._id, name: findUser.name }
+    const token = await this.jwtService.sign(payload)
+
+    //gather all the data and returnign it
+    const data = {
+      user: findUser,
+      token
+    }
     return data;
   }
-
 }
